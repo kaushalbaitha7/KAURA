61f17c298e7328f5a56fc08deb5cf331c58b1795
kaushalbaitha7 <kaushalbaitha74@gmail.com>
2026-05-29 03:12:26 +0530

    Kaura webiste frontend version

---
+++ script.js
@@ -0,0 +1,106 @@
+
+const aboutModal =
+document.getElementById("aboutModal");
+
+const supportModal =
+document.getElementById("supportModal");
+
+const comingPopup =
+document.getElementById("comingPopup");
+
+
+function openProject(link){
+
+
+  if(link === "#"){
+
+    comingPopup.style.display = "flex";
+
+  }
+
+ 
+  else{
+
+    window.open(link, "_blank");
+
+  }
+
+}
+
+
+function closeComingPopup(){
+
+  comingPopup.style.display = "none";
+
+}
+
+
+
+function openAbout(){
+
+  aboutModal.style.display = "flex";
+
+}
+
+function closeAbout(){
+
+  aboutModal.style.display = "none";
+
+}
+
+
+
+function openSupport(){
+
+  supportModal.style.display = "flex";
+
+}
+
+function closeSupport(){
+
+  supportModal.style.display = "none";
+
+}
+
+
+
+window.addEventListener("click", function(e){
+
+
+  if(e.target === aboutModal){
+
+    aboutModal.style.display = "none";
+
+  }
+
+ 
+  if(e.target === supportModal){
+
+    supportModal.style.display = "none";
+
+  }
+
+  
+  if(e.target === comingPopup){
+
+    comingPopup.style.display = "none";
+
+  }
+
+});
+
+
+
+document.addEventListener("keydown", function(e){
+
+  if(e.key === "Escape"){
+
+    aboutModal.style.display = "none";
+
+    supportModal.style.display = "none";
+
+    comingPopup.style.display = "none";
+
+  }
+
+});
